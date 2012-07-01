1a8ea74e7219bed18f46049f30c01470374cca6f
George Pantazis <gpantazis@cpbgroup.com>
2012-07-01 12:54:16 -0600

    Initial checkin

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ node-backbone
 =============
 
 Experimental package to give (very very) basic Backbone structures to node projects.
+
+##Changelog
+
+* v0.1.0 Initial release
+
+## License
+Copyright (c) 2012 George Pantazis
+Licensed under the MIT license.
---
+++ classreset.js
@@ -0,0 +1,95 @@
+
+var	Backbone = require('backbone'),
+	_ = require('underscore');
+
+// Reset functionality of Backbone.View
+
+var View = Backbone.View = function(options) {
+	this.cid = _.uniqueId('View');
+	this._configure(options || {});
+
+	if (!this.route) {
+		console.log('Error: When instantiating a new View, route was not provided.');
+		return;
+	}
+
+	this.initialize.apply(this, arguments);
+};
+
+var viewOptions = ['route'];
+
+_.extend(View.prototype, null, {
+
+	initialize: function(){},
+
+	_configure: function(options) {
+		if (this.options) options = _.extend({}, this.options, options);
+		for (var i = 0, l = viewOptions.length; i < l; i++) {
+			var attr = viewOptions[i];
+			if (options[attr]) this[attr] = options[attr];
+		}
+		this.options = options;
+	}
+
+});
+
+// Reset functionality of Backbone.Router
+
+var Router = Backbone.Router = function(options) {
+	this.cid = _.uniqueId('Router');
+	this._configure(options || {});
+	this.initialize.apply(this, arguments);
+};
+
+var routerOptions = ['attributes'];
+
+_.extend(Router.prototype, null, {
+
+	initialize: function(){},
+
+	_configure: function(options) {
+		if (this.options) options = _.extend({}, this.options, options);
+		for (var i = 0, l = routerOptions.length; i < l; i++) {
+			var attr = routerOptions[i];
+			if (options[attr]) this[attr] = options[attr];
+		}
+		this.options = options;
+	}
+
+});
+
+// Shared Extension Functions
+
+var extend = function (protoProps, classProps) {
+	var child = inherits(this, protoProps, classProps);
+	child.extend = this.extend;
+	return child;
+};
+
+View.extend = Router.extend = extend;
+
+var ctor = function(){};
+
+var inherits = function(parent, protoProps, staticProps) {
+
+	var child;
+
+	if (protoProps && protoProps.hasOwnProperty('constructor')) {
+		child = protoProps.constructor;
+	} else {
+		child = function(){ parent.apply(this, arguments); };
+	}
+
+	_.extend(child, parent);
+
+	ctor.prototype = parent.prototype;
+	child.prototype = new ctor();
+
+	if (protoProps) _.extend(child.prototype, protoProps);
+	if (staticProps) _.extend(child, staticProps);
+
+	child.prototype.constructor = child;
+	child.__super__ = parent.prototype;
+
+	return child;
+};
